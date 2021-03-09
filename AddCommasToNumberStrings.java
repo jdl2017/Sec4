9364bcc436ec320a33893a8b0149433018c170b1
Bilal Khan <jandhal@gmail.com>
2021-03-09 10:03:12 -0600

    Sec 4 Assignment

---
+++ AddCommasToNumberStrings.java
@@ -0,0 +1,29 @@
+package week3;
+
+import acm.program.ConsoleProgram;
+
+public class AddCommasToNumberStrings extends ConsoleProgram {
+	
+	private String addCommasToNumericString(String digits) {
+		String result = "";
+		int len = digits.length();
+		int nDigits = 0;
+		for (int i = len - 1; i >= 0; i--) {
+			result = digits.charAt(i) + result;
+			nDigits++;
+			if (((nDigits % 3) == 0) && (i > 0)) {
+			result = "," + result;
+			}
+		}
+		return result;
+	}
+	
+	public void run() {
+		while (true) {
+			String digits = readLine("Enter a number: "); 
+			if (digits.length() == 0) break; 
+			println(addCommasToNumericString(digits));
+			}
+	}
+	
+}
---
+++ DeleteCharactersFromString.java
@@ -0,0 +1,24 @@
+package week3;
+
+import acm.program.ConsoleProgram;
+
+public class DeleteCharactersFromString extends ConsoleProgram {
+	
+	private String removeAllOccurrences(String str, char ch) {
+		String result = "";
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) != ch) {
+			result += str.charAt(i);	
+			}
+		}
+	    return result;
+	}
+	
+	public void run() {
+		println(removeAllOccurrences("This is a test", 't'));
+		println(removeAllOccurrences("Summer is here!", 'e'));
+		println(removeAllOccurrences("---0---", '-'));
+			}
+
+	}
+	
